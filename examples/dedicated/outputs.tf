ccdaa96d8906ccfcc81ad2f45a6cf0ba4ede05bb
matttrach <matttrach@gmail.com>
2023-10-31 11:48:50 -0500

    fix: typo in variable
    Signed-off-by: matttrach <matttrach@gmail.com>

--- examples/dedicated/outputs.tf
+++ examples/dedicated/outputs.tf
@@ -6,6 +6,6 @@ output "ssh" {
   value = merge(
     { "${local.names[0]}" = "${local.username}@${module.InitialServer.server_public_ip}" },
     { for i in range(1, local.server_count) : local.names[i] => "${local.username}@${module.Servers[local.names[i]].server_public_ip}" },
-    { for i in range(local.server_count, (local.server_count + local.local.agent_count)) : local.names[i] => "${local.username}@${module.Agents[local.names[i]].server_public_ip}" }
+    { for i in range(local.server_count, (local.server_count + local.agent_count)) : local.names[i] => "${local.username}@${module.Agents[local.names[i]].server_public_ip}" }
   )
 }
