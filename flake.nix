9002040a1a283166a0ca3cbd697687c1c23fadb5
matttrach <matttrach@gmail.com>
2023-11-07 16:04:23 -0600

    fix: add terraform and leftovers to nix, add nix to ci, cleanup workflows
    Signed-off-by: matttrach <matttrach@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   # validate a flake with 'nix flake check .'
   # alias the use of flakes with: "alias nix='nix --extra-experimental-features nix-command --extra-experimental-features flakes'"
-  #  you can also set a config file at ~/.config/nix/nix.conf or /etc/nix.conf, but I wanted to remove that dependency
 
+  # WARNING! this is linux/mac only!
   description = "A reliable testing environment";
 
   # https://status.nixos.org/ has the latest channels, it is recommended to use a commit hash
@@ -19,29 +19,46 @@
   #inputs.nixpkgs.follows = "nixpkgs/0228346f7b58f1a284fdb1b72df6298b06677495";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-terraform.url = "github:/NixOS/nixpkgs/39ed4b64ba5929e8e9221d06b719a758915e619b";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, nixpkgs-terraform, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       # 'legacy' is not bad, it looks for previously imported nixpkgs
       #  this allows idempotent loading of nixpkgs in dependent flakes
       # https://discourse.nixos.org/t/using-nixpkgs-legacypackages-system-vs-import/17462/8
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        pkgs-terraform = nixpkgs-terraform.legacyPackages.${system};
+
+        # get "leftovers" bin from release and add it to shell
+        leftovers-urls = {
+          "x86_64-linux" = "https://github.com/genevieve/leftovers/releases/download/v0.70.0/leftovers-v0.70.0-linux-amd64";
+          "x86_64-darwin" = "https://github.com/genevieve/leftovers/releases/download/v0.70.0/leftovers-v0.70.0-darwin-amd64";
+          "aarch64-darwin" = "https://github.com/genevieve/leftovers/releases/download/v0.70.0/leftovers-v0.70.0-darwin-arm64";
+        };
+        leftovers = pkgs.runCommand "leftovers-v0.70.0" {} ''
+          cp ${pkgs.fetchurl {
+            url = leftovers-urls."${system}";
+            sha256 = "sha256-HV12kHqB14lGDm1rh9nD1n7Jvw0rCnxmjC9gusw7jfo=";
+          }} $out
+          chmod +x $out
+        '';
+        leftovers-wrapper = pkgs.writeShellScriptBin "leftovers" ''
+          exec ${leftovers} "$@"
+        '';
+
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; with pkgs-terraform; [
+          buildInputs = with pkgs; [
             bashInteractive
             git
-            terraform
             tflint
             shellcheck
+            tfswitch
           ];
           shellHook = ''
-            source .envrc
+            tfswitch 1.5.7;
+            export PATH="$PATH:${leftovers-wrapper}/bin";
           '';
         };
       }
