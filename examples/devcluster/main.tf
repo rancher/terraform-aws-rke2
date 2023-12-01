d461a3b2b81da5c548c03cfe90b7b905c1cac36c
Matt Trachier <matttrach@gmail.com>
2023-12-01 15:57:44 -0600

    fix: use the file function to get the extra config content (#52)
    Signed-off-by: matttrach <matttrach@gmail.com>

--- examples/devcluster/main.tf
+++ examples/devcluster/main.tf
@@ -14,17 +14,18 @@ locals {
   owner        = "terraform-ci@suse.com" # put your email here
   ssh_key_name = var.ssh_key_name        # this must already exist in AWS, it must have a tag with 'Name' = <this value>
   # ssh_key_content       = file(var.ssh_key_path) # if you want the module to create the keypair object in AWS for you, specify the contents of the public key like this
-  rke2_version       = var.rke2_version # the exact release tag, eg. v1.28.2+rke2r1
-  identifier         = var.identifier   # simple random string to identify resources
-  email              = "terraform-ci@suse.com"
-  name               = "tf-aws-rke2-dev-${local.identifier}"
-  username           = "tf-${local.identifier}"              # WARNING: This must be less than 32 characters!
-  server_prep_script = file("${abspath(path.root)}/prep.sh") # a script that will run before start, after install
-  configs            = "${abspath(path.root)}/config"        # add custom configs here, this is added to all servers, see "dedicated" example to place different configs on different nodes
-  cluster_size       = 3                                     # this example provisions all servers the same, see the "dedicated" example to alter configs and roles
-  prefix             = "tf-devcluster-${local.identifier}"   # this can be anything you want, it makes it marginally easier to find in AWS
-  server_type        = "small"                               # https://github.com/rancher/terraform-aws-server/blob/main/modules/server/types.tf
-  image_type         = "rhel-8"                              # https://github.com/rancher/terraform-aws-server/blob/main/modules/image/types.tf
+  rke2_version         = var.rke2_version # the exact release tag, eg. v1.28.2+rke2r1
+  identifier           = var.identifier   # simple random string to identify resources
+  email                = "terraform-ci@suse.com"
+  name                 = "tf-aws-rke2-dev-${local.identifier}"
+  username             = "tf-${local.identifier}"              # WARNING: This must be less than 32 characters!
+  server_prep_script   = file("${abspath(path.root)}/prep.sh") # a script that will run before start, after install
+  configs              = "${abspath(path.root)}/config"        # add custom configs here, this is added to all servers, see "dedicated" example to place different configs on different nodes
+  cluster_size         = 3                                     # this example provisions all servers the same, see the "dedicated" example to alter configs and roles
+  prefix               = "tf-devcluster-${local.identifier}"   # this can be anything you want, it makes it marginally easier to find in AWS
+  server_type          = "small"                               # https://github.com/rancher/terraform-aws-server/blob/main/modules/server/types.tf
+  image_type           = "rhel-8"                              # https://github.com/rancher/terraform-aws-server/blob/main/modules/image/types.tf
+  extra_config_content = file("${local.configs}/config.yaml")
 
   # We are generating random names for the servers here, you might want to simplify this for your use case, just pick some names
   # Keep in mind that these names must not be generated using resources, but they can use functions and expressions
@@ -76,15 +77,16 @@ module "InitialServer" {
   ssh_username        = local.username
   ssh_key_name        = local.ssh_key_name
   # ssh_key_content     = local.ssh_key_content
-  local_file_path     = "${abspath(path.root)}/${local.names["0"]}"
-  rke2_version        = local.rke2_version
-  join_token          = random_uuid.join_token.result
-  install_method      = "rpm" # requires "egress" security group type
-  skip_download       = true
-  retrieve_kubeconfig = true
-  server_type         = local.server_type
-  image_type          = local.image_type
-  server_prep_script  = local.server_prep_script
+  local_file_path      = "${abspath(path.root)}/${local.names["0"]}"
+  rke2_version         = local.rke2_version
+  join_token           = random_uuid.join_token.result
+  install_method       = "rpm" # requires "egress" security group type
+  skip_download        = true
+  retrieve_kubeconfig  = true
+  server_type          = local.server_type
+  image_type           = local.image_type
+  server_prep_script   = local.server_prep_script
+  extra_config_content = local.extra_config_content
 }
 
 module "Servers" {
@@ -96,24 +98,25 @@ module "Servers" {
   ]
   source = "../../" # change this to "rancher/rke2/aws" per https://registry.terraform.io/modules/rancher/rke2/aws/latest
   # version = "v0.0.7" # when using this example you will need to set the version
-  for_each            = toset([for i in range(1, local.cluster_size) : local.names[tostring(i)]]) # "1","2","3"... less than cluster_size
-  name                = each.key
-  owner               = local.email
-  vpc_name            = local.names["0"] # reuse what we generated with initial server
-  subnet_name         = each.key         # each server gets its own subnet
-  subnet_cidr         = local.subnet_cidrs[each.key]
-  availability_zone   = local.availability_zones[each.key]
-  security_group_name = local.names["0"] # reuse what we generated with initial server
-  ssh_username        = local.username
-  ssh_key_name        = local.ssh_key_name # reuse what we generated with initial server
-  local_file_path     = local.file_paths[each.key]
-  rke2_version        = local.rke2_version
-  join_token          = random_uuid.join_token.result
-  join_url            = module.InitialServer.join_url
-  install_method      = "rpm"
-  skip_download       = true
-  retrieve_kubeconfig = false # we can reuse the kubeconfig downloaded with the initial server
-  server_type         = local.server_type
-  image_type          = local.image_type
-  server_prep_script  = local.server_prep_script
+  for_each             = toset([for i in range(1, local.cluster_size) : local.names[tostring(i)]]) # "1","2","3"... less than cluster_size
+  name                 = each.key
+  owner                = local.email
+  vpc_name             = local.names["0"] # reuse what we generated with initial server
+  subnet_name          = each.key         # each server gets its own subnet
+  subnet_cidr          = local.subnet_cidrs[each.key]
+  availability_zone    = local.availability_zones[each.key]
+  security_group_name  = local.names["0"] # reuse what we generated with initial server
+  ssh_username         = local.username
+  ssh_key_name         = local.ssh_key_name # reuse what we generated with initial server
+  local_file_path      = local.file_paths[each.key]
+  rke2_version         = local.rke2_version
+  join_token           = random_uuid.join_token.result
+  join_url             = module.InitialServer.join_url
+  install_method       = "rpm"
+  skip_download        = true
+  retrieve_kubeconfig  = false # we can reuse the kubeconfig downloaded with the initial server
+  server_type          = local.server_type
+  image_type           = local.image_type
+  server_prep_script   = local.server_prep_script
+  extra_config_content = local.extra_config_content
 }
