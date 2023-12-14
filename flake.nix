f0d300437231090d0da5cf403c2d73561467131d
Matt Trachier <matttrach@gmail.com>
2023-12-14 10:39:55 -0600

    fix: introduce leftovers and configure updatecli to sign commits (#77)
    Signed-off-by: matttrach <matttrach@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
-  # validate a flake with 'nix flake check .'
-  # alias the use of flakes with: "alias nix='nix --extra-experimental-features nix-command --extra-experimental-features flakes'"
-
   # WARNING! this is linux/mac only!
+  # tested on x86_64 MacBook Pro Sonoma and "ubuntu-latest" GitHub runner (linux x86_64)
   description = "A reliable testing environment";
 
   # https://status.nixos.org/ has the latest channels, it is recommended to use a commit hash
@@ -29,32 +27,67 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
+        # get "updatecli" tar from release, unpack it, and add it to shell
+        updatecli-version = {
+          "selected" = "v0.66.0";
+        };
+        updatecli-prep = {
+          "x86_64-darwin"   = {
+            "url" = "https://github.com/updatecli/updatecli/releases/download/${updatecli-version.selected}/updatecli_Darwin_x86_64.tar.gz";
+            "sha" = "sha256-AkYYaCp/a4KkZ4zcYc3GepllyPE6bHb7x7K25JSyljY=";
+          };
+          "aarch64-darwin"  = {
+            "url" = "https://github.com/updatecli/updatecli/releases/download/${updatecli-version.selected}/updatecli_Darwin_arm64.tar.gz";
+            "sha" = "sha256-6dEz3DYrq+hQFVby6TmILWkMF3nL52mHcCoQtt5q6YM=";
+          };
+          "x86_64-linux"    = {
+            "url" = "https://github.com/updatecli/updatecli/releases/download/${updatecli-version.selected}/updatecli_Linux_x86_64.tar.gz";
+            "sha" = "sha256-tmboI0ew+LApo3uLVqebaa8VA/6rgonGJH2onQEbSyk=";
+          };
+        };
+        updatecli = pkgs.runCommand "updatecli-${updatecli-version.selected}" {} ''
+          cp ${pkgs.fetchzip { # when fetching archives use fetchzip instead of fetchurl to automatically unpack
+            url = updatecli-prep."${system}".url;
+            sha256 = updatecli-prep."${system}".sha;
+            stripRoot = false;
+          }}/updatecli $out
+          chmod +x $out
+        '';
+        updatecli-wrapper = pkgs.writeShellScriptBin "updatecli" ''
+          exec ${updatecli} "$@"
+        '';
+
         # get "leftovers" bin from release and add it to shell
         leftovers-version = {
           # remember when updating the version to also update the shas
           # to get the sha, download the file and run 'nix hash file <file>'
           "selected" = "v0.70.0";
         };
-        leftovers-urls = {
-          "x86_64-darwin" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-darwin-amd64";
-          "aarch64-darwin" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-darwin-arm64";
-          "x86_64-linux" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-linux-amd64";
-        };
-        leftovers-shas = {
-          "x86_64-linux" = "sha256-D2OPjLlV5xR3f+dVHu0ld6bQajD5Rv9GLCMCk9hXlu8=";
-          "x86_64-darwin" = "sha256-HV12kHqB14lGDm1rh9nD1n7Jvw0rCnxmjC9gusw7jfo=";
-          "aarch64-darwin" = "sha256-Tw7G538RYZrwIauN7kI68u6aKS4d/0Efh+dirL/kzoM=";
+        leftovers-prep = {
+          "x86_64-darwin"   = {
+            "url" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-darwin-amd64";
+            "sha" = "sha256-HV12kHqB14lGDm1rh9nD1n7Jvw0rCnxmjC9gusw7jfo=";
+          };
+          "aarch64-darwin"  = {
+            "url" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-darwin-arm64";
+            "sha" = "sha256-HV12kHqB14lGDm1rh9nD1n7Jvw0rCnxmjC9gusw7jfo=";
+          };
+          "x86_64-linux"    = {
+            "url" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-linux-amd64";
+            "sha" = "sha256-D2OPjLlV5xR3f+dVHu0ld6bQajD5Rv9GLCMCk9hXlu8=";
+          };
         };
         leftovers = pkgs.runCommand "leftovers-${leftovers-version.selected}" {} ''
           cp ${pkgs.fetchurl {
-            url = leftovers-urls."${system}";
-            sha256 = leftovers-shas."${system}";
+            url = leftovers-prep."${system}".url;
+            sha256 = leftovers-prep."${system}".sha;
           }} $out
           chmod +x $out
         '';
         leftovers-wrapper = pkgs.writeShellScriptBin "leftovers" ''
           exec ${leftovers} "$@"
         '';
+
       in
       {
         devShells.default = pkgs.mkShell {
@@ -66,17 +99,21 @@
             git
             gnupg
             go
+            jq
             less
+            nix
             openssh
             shellcheck
             tflint
             tfswitch
           ];
           shellHook = ''
-            rm -rf "/usr/local/bin/switched-terraform"
-            install -d "/usr/local/bin/switched-terraform"
-            tfswitch -b "/usr/local/bin/switched-terraform/terraform" -d "1.5.7" 1.5.7 > /dev/null;
-            export PATH="$PATH:${leftovers-wrapper}/bin:/usr/local/bin/switched-terraform";
+            # use tfswitch to create a terraform bin of the proper version and add it to the path
+            rm -rf "/usr/local/bin/switched-terraform";
+            install -d "/usr/local/bin/switched-terraform";
+            tfswitch -b "/usr/local/bin/switched-terraform/terraform" -d "1.5.7" 1.5.7 &>/dev/null;
+            export PATH="$PATH:${updatecli-wrapper}/bin:${leftovers-wrapper}/bin:/usr/local/bin/switched-terraform";
+            alias nix='nix --extra-experimental-features nix-command --extra-experimental-features flakes';
           '';
         };
       }
