3dfeec70f6ddfeeb10dac2ff34f7cc961a66ff42
Matt Trachier <matt.trachier@suse.com>
2024-03-08 18:11:46 -0600

    fix: use zipmap to avoid ambiguous map
    Signed-off-by: Matt Trachier <matt.trachier@suse.com>

--- examples/dedicated/outputs.tf
+++ examples/dedicated/outputs.tf
@@ -4,7 +4,7 @@ output "kubeconfig" {
 }
 output "ssh" {
   value = merge(
-    { local.names[0] = "${local.username}@${module.InitialServer.server_public_ip}" },
+    zipmap([local.names[0]], ["${local.username}@${module.InitialServer.server_public_ip}"]),
     { for i in range(1, local.server_count) : local.names[i] => "${local.username}@${module.Servers[local.names[i]].server_public_ip}" },
     { for i in range(local.server_count, (local.server_count + local.agent_count)) : local.names[i] => "${local.username}@${module.Agents[local.names[i]].server_public_ip}" }
   )
