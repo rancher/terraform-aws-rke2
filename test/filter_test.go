8f5cdf36041eeb77355c151f13d637f47b6d8139
Matt Trachier <matttrach@gmail.com>
2026-08-03 18:24:28 -0500

    fix: support --no-color flag in test runner script (#280)

--- test/filter_test.go
+++ test/filter_test.go
@@ -2,6 +2,8 @@ package test
 
 import (
 	"fmt"
+	"os/exec"
+	"strings"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
@@ -10,4 +12,24 @@ import (
 func TestFilter(_ *testing.T) {
 	f := types.Filter{}
 	fmt.Printf("%T\n", f.Values)
+}
+
+func TestRunTestsNoColorHelp(t *testing.T) {
+	cmd := exec.CommandContext(t.Context(), "bash", "../run_tests.sh", "--no-color", "--help")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to run run_tests.sh: %v. Output:\n%s", err, string(out))
+	}
+
+	outputStr := string(out)
+
+	// Verify --no-color is documented in the help output
+	if !strings.Contains(outputStr, "--no-color") {
+		t.Errorf("Expected help output to contain '--no-color', but got:\n%s", outputStr)
+	}
+
+	// Verify the output contains no ANSI escape codes
+	if strings.Contains(outputStr, "\x1b[") || strings.Contains(outputStr, "\033[") {
+		t.Errorf("Expected output to have no color escape codes, but found ANSI escape sequences in:\n%s", outputStr)
+	}
 }
