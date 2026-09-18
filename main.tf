d4a56b8bcd22d6a26cdf5f99888f131248c64639
Matt Trachier <matttrach@gmail.com>
2026-09-18 17:48:40 -0500

    fix: update install module (#298)
    Signed-off-by: Matt Trachier <matt.trachier@suse.com>

--- main.tf
+++ main.tf
@@ -405,7 +405,7 @@ module "install" {
     module.download,
   ]
   source                     = "rancher/rke2-install/null"
-  version                    = "v1.3.5"
+  version                    = "v1.3.6"
   release                    = local.install_rke2_version
   rpm_channel                = local.install_rpm_channel
   local_file_path            = local.local_file_path
